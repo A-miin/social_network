d19438aa8a6ce1c8d3f787fd512d5c566bfe04c9
A-miin <zulaykaisaeva@gmail.com>
2022-06-02 09:11:13 +0300

    Add user register

---
+++ accounts/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+
+UserModel = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = UserModel
+        fields = (
+            'username',
+            'password',
+            'password2',
+        )
+
+    def create(self, validated_data):
+        user = UserModel.objects.create(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+
+        return user
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        attrs['password'] = make_password(attrs['password'])
+
+        return attrs
