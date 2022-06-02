03b9064fc7718448df1c67bef2afc83d3f3b097a
A-miin <zulaykaisaeva@gmail.com>
2022-06-02 16:08:03 +0300

    Add README

--- accounts/tests.py
+++ accounts/tests.py
@@ -9,21 +9,12 @@ class ValidateDataTest(APITestCase):
 
     def test_registration_with_valid_data(self):
         data = {
-            "username": "user1db",
+            "username": "user1@user1.user",
             "password": "definiton100",
             "password2": "definiton100"
         }
         response = self.client.post(reverse('sign-up'), data=json.dumps(data), content_type='application/json')
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
-
-    def test_registration_with_diff_pass(self):
-        data = {
-            "username": "user",
-            "password": "definiton100",
-            "password1": "definiton101"
-        }
-        response = self.client.post(reverse('sign-up'), data=json.dumps(data), content_type='application/json')
-        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     def test_registration_with_empty_field(self):
         data = {
